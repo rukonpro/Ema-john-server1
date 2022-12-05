51ce2c806461845608285708c7dbdda4d57e44a2
Your Name <you@example.com>
2022-12-05 22:44:28 +0600

    codepush

--- index.js
+++ index.js
@@ -107,7 +107,6 @@ async function run() {
 }
 run().catch(console.dir)
 
-
 // -------------------------------------------------
 app.get('/', (req, res) => {
     res.send("Ema jon server Running ")
